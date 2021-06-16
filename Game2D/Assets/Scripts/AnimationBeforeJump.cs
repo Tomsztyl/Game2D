640cd27e1af1b2bd4bd56d73e1b91d56465a2791
Tomsztyl <tomek.jelito@gmail.com>
2021-06-16 21:36:10 +0200

    Animation Before Jumping
    Animation Checked if animation [Before_Jump] is ending. If  animation is ending in component animator change variable

---
+++ Game2D/Assets/Scripts/AnimationBeforeJump.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class AnimationBeforeJump : StateMachineBehaviour
+{
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        animator.SetBool("isBeforeJumpingUp",true);
+    }
+}

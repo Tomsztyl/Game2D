de5e416b33ea29c5688128a9ff1e4fc02611abaa
Tomsztyl <tomek.jelito@gmail.com>
2021-06-26 17:10:09 +0200

    Using "Attack" /Attack mechanics
    Adding attack mechanics by assigning an action from the "Input System" and interrupting the attack

---
+++ Game2D/Assets/Scripts/AttackController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace Attack
+{
+    public class AttackController
+    {
+        public void AttackInputCharacter(InputActionPlayer inputActions, Animator animatorCharacter, string parametrsAttack)
+        {
+            if (inputActions != null)
+            {
+                //is Input System
+                inputActions.PlayerAttack.Attack.started += ctxbtn => Attack(animatorCharacter, parametrsAttack, true);
+                inputActions.PlayerAttack.Attack.canceled += ctxbtn => Attack(animatorCharacter, parametrsAttack, false);
+
+            }
+            else
+                //no define input system Debug [Error]
+                Debug.LogError("Input System Attack is not define");
+        }
+        public void Attack(Animator animatorCharacter, string parametrsAttack, bool isAttack)
+        {
+            AttackAnimationSwitch(animatorCharacter, parametrsAttack, isAttack);
+        }
+        private void AttackAnimationSwitch(Animator animatorAttack, string parametrsAttack, bool isAttack)
+        {
+            if (animatorAttack != null)
+                animatorAttack.SetBool(parametrsAttack, isAttack);
+        }
+    }
+}
